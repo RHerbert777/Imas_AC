7e52a622ae75f41c17cd34d02866027c3f5bc6b5
Herbert Renner Silva Araújo <rennerherbert7@gmail.com>
2025-12-22 16:08:34 -0300

    Reorganização

--- main.c
+++ main.c
@@ -1,7 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Imas.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+
+#define IMAS_MEM_SIZE 4096
+
+/* Opcodes do IMAS */
+#define IMAS_HALT      0x0
+#define IMAS_LOAD_M    0x1
+#define IMAS_LOAD_MQ   0x2
+#define IMAS_LOAD_MQ_M 0x3
+#define IMAS_STOR_M    0x4
+#define IMAS_STA_M     0x5
+#define IMAS_ADD_M     0x6
+#define IMAS_SUB_M     0x7
+#define IMAS_MUL_M     0x8
+#define IMAS_DIV_M     0x9
+#define IMAS_JMP_M     0xA
+#define IMAS_JZ_M      0xB
+#define IMAS_JNZ_M     0xC
+#define IMAS_JPOS_M    0xD
+#define IMAS_IN        0xE
+#define IMAS_OUT       0xF
+
+/* Estrutura do Processador */
+typedef struct {
+    // Unidade de Controle
+    uint16_t pc;    // Program Counter - Endereço da proxima instrução.
+    uint16_t mar;   // Memory Address Register - Endereço a ser acesado pela memoria. 
+    uint16_t ibr;   // Instruction Buffer Register - Mantem toda a instrução.
+    uint16_t ir;    // Instruction Register - Mantem o opcode da instrução.
+
+    // Unidade Lógica e Aritmética
+    int16_t mbr;    // Memory Buffer Register - Dado a ser lido/escrito na memoria.
+    int16_t ac;     // Accumulator - acm.
+    int16_t mq;     // Multiplier Quotient - usado na multiplicação e divisão.
+
+    // Memória
+    uint16_t memory[IMAS_MEM_SIZE];
+} imas_t;
+
+//Recebe a estrutura e inicializa os estados dos registradores zerados.
+void imas_init(imas_t *imas){
+	imas->pc = 0;
+    imas->mar = 0;
+    imas->ibr = 0;
+    imas->ir = 0;
+    imas->mbr = 0;
+    imas->ac = 0;
+    imas->mq = 0;
+    
+    // Zera a memória de forma segura (2 bytes por posição)
+    memset(imas->memory, 0, IMAS_MEM_SIZE * sizeof(uint16_t));
+}
+
+void memory_read(imas_t *imas);
+
+void memory_write(imas_t *imas, bool modify_address);
+
+void io_read(imas_t *imas);
+
+void io_write(imas_t *imas);
+
+void step(imas_t *imas); // Executa um ciclo de instrução
 
 int main(int argc, char *argv[]) {
 	/* Check arguments */
@@ -36,7 +99,9 @@ int main(int argc, char *argv[]) {
 		imas.memory[address] = buffer;
     }
 
+
 	/* Processor running */
+	//Enquanto falso faça
 	bool imas_halt = false;
 	do {
 		/* PC before modifications */
